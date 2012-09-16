63e52514d327f71b528f6bc50dc912d16b606039
Mattia Padovani <padovani.mattia@gmail.com>
2012-09-16 14:50:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Steak::Application.config.secret_token = '3c846568586a7287d4cfbc268f0f1b4ced5a614447fede1c089de19e70b5d1188f0b10cb1a82b5f412b4a347efaaa46f4552a166e199d7e27fcdaa705707829f'
